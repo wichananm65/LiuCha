371824f0da33a591384256d5325b5b1592d00605
Wichanan <wichananm65@nu.ac.th>
2024-10-29 16:54:42 +0700

    9

--- SaleHistory.java
+++ SaleHistory.java
@@ -3,37 +3,36 @@ import java.time.LocalDate;
 public class SaleHistory {
     //attribute
     private int saId;
-    private LocalDate date;
     private double totalPrice;
+    private int receiptCount;
     
     //composition
     private Receipt[] receipts;
 
     public SaleHistory(LocalDate date) {
-        this.date = date;
         totalPrice = 0;
+        this.receipts = new Receipt[10];
+        this.receiptCount = 0;
     }
 
     public int getSaId(){
         return saId;
     }
-    public String getDate(){
-        return date.toString();
-    }
 
     public double getTotalPrice(){
         return totalPrice;
     }
-    
-    public String showAllReceipt(){
-        String text = "";
-        for(int i=0;i<receipts.length;i++){
-            text = text + receipts[i].showReceipt() + "\n";
-        }
-        return text;
-    }
 
-    public String showSaleHistory(){
-        return "Date " + date + " Total Sale: " + totalPrice + " Baht\n" + showAllReceipt();
+    public void saveReceipt(Receipt receipt){
+        this.receipts[receiptCount] = receipt;
+        receiptCount++;
+    }
+    
+    public void showAllReceipt(){
+        for(int i=0;i<receipts.length;i++){
+            if(receipts[i] != null){
+                receipts[i].showReceipt();
+            }
+        }
     }
 }
